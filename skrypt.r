75df996e1a9a6dbf052a358586fb6a1015fcf5b8
Cyryl Waśkieiwcz <cyryl.was@gmail.com>
2018-03-08 19:03:06 +0100

    update

--- skrypt.r
+++ skrypt.r
@@ -31,5 +31,11 @@ p2 = 2.78e-11
 sigma1 = calc_sigma(n1, p1, mean_x1)
 sigma2 = calc_sigma(n2, p2, mean_x2)
 
-sigma1
-sigma2
+przedzial_predykacji <- function(n, m, alfa, mean_x, sigma) {
+    t = qt(1 - alfa, n - 1)
+    delta = t * sigma * sqrt(1 / n + 1 / m)
+    return(c(mean_x - delta, mean_x + delta))
+}
+
+przedzial_predykacji(n1, 1, 0.2, mean_x1, sigma1)
+przedzial_predykacji(n1, 1, 0.33, mean_x1, sigma1)
