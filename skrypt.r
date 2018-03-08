e6dd3af3c348e236e69baf9af473dc05ea46e65f
Cyryl Waśkieiwcz <cyryl.was@gmail.com>
2018-03-08 18:09:38 +0100

    update

--- skrypt.r
+++ skrypt.r
@@ -1,4 +1,6 @@
 #to czyta dane z plików i wczytuje je do "zmiennych" dane1, dane2
 dane1 = read.csv("wprowadz_1.csv")
 dane2 = read.csv("wprowadz_2.csv")
+t.test(dane1)
+t.test(dane2)
 t.test(dane1, dane2)
