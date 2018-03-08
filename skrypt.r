ce05f17f98e5af7f4d46d4466118f5fc5867e29f
Cyryl Waśkieiwcz <cyryl.was@gmail.com>
2018-03-08 18:42:07 +0100

    update

--- skrypt.r
+++ skrypt.r
@@ -13,7 +13,23 @@ t.test(dane1, dane2, var.equal=TRUE)
 "#############################"
 "HISTORIA ZMYŚLONA"
 
-calc_sigma <- function(n, p, mean_x)
-{
-  
+calc_sigma <- function(n, p, mean_x) {
+  t0 = qt(1 - p / 2, n - 1)
+  #t0 = mean_x * sqrt(n) / sigma
+  sigma = mean_x * sqrt(n) / t0
+  return (sigma)
 }
+
+n1 = 10
+mean_x1 = 1050
+p1 = 0.031
+
+n2 = 100
+mean_x2 = 900
+p2 = 2.78e-11
+
+sigma1 = calc_sigma(n1, p1, mean_x1)
+sigma2 = calc_sigma(n2, p2, mean_x2)
+
+sigma1
+sigma2
